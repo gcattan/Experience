321587f8c8d68fbcf5db6d14f4fe8f2b6d0e525a
gcattan <gcattan@hotmail.fr>
2021-02-26 18:55:48 +0100

    starting test for xp_array

---
+++ tests/xp_array_test.c
@@ -0,0 +1,26 @@
+﻿#include "../common/xp_array.h"
+#include <stdio.h>
+
+#define RUN(X) \
+    ++nbTest;  \
+    if (!X())  \
+        ++nbFailed;
+#define ASSERT(CONDITION) \
+    if (!CONDITION)       \
+        return FALSE;
+
+bool test_array_push_shouldNotBeEmpty()
+{
+    xp_array *array = xp_array_create();
+    int *a = NEW(int);
+    array->push(a);
+    ASSERT(array->len == 1)
+    return TRUE;
+}
+
+void test_array()
+{
+    int nbTest = 0;
+    int nbFailed = 0;
+    RUN(test_array_push_shouldNotBeEmpty);
+}
